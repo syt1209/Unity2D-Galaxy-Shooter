b94fc697d1f258692e5dc1b0f3c77d534cc945c6
syt1209 <syt1209@gmail.com>
2021-03-23 22:23:13 -0400

    Player starting position

---
+++ GalaxyShooter_2D/Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // state variables
+    private Vector3 _startPos = new Vector3(0, 0, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = _startPos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
