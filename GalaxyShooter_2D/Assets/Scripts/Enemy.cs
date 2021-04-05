2c10cce607fc7127d267e85f7441835c7f159b58
syt1209 <syt1209@gmail.com>
2021-04-04 22:27:07 -0400

    Enemy setup and movement

---
+++ GalaxyShooter_2D/Assets/Scripts/Enemy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // state variables
+    [SerializeField] private float _enemySpeed = 4.0f;
+    private float _yMin = -5f, _yMax = 7f, _xMin = -8f, _xMax = 8f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveDown();
+    }
+
+    private void MoveDown()
+    {
+        transform.Translate(Vector3.down * _enemySpeed * Time.deltaTime);
+
+        if (transform.position.y < _yMin)
+        {
+            float _randomX = Random.Range(_xMin, _xMax);
+            transform.position = new Vector3(_randomX, _yMax, 0);
+        }
+    }
+}
