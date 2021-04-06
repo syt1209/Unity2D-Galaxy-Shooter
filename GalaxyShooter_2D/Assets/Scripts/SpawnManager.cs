ed8c04236d73f00d8fe80e07d540ec3b824ae7fb
syt1209 <syt1209@gmail.com>
2021-04-06 18:24:31 -0400

    Spawn routine set up

---
+++ GalaxyShooter_2D/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+
+    // cached reference
+    [SerializeField]
+    private GameObject _enemyPrefab;
+    [SerializeField]
+    private WaitForSeconds _enemySpawnDelay = new WaitForSeconds(3.0f);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(EnemySpawnRoutine());
+    }
+
+    private IEnumerator EnemySpawnRoutine()
+    {
+        while (true)
+        {
+            Vector3 posToSpawn = new Vector3(Random.Range(-8f, 8f), 7, 0);
+            GameObject _newEnemy = Instantiate(_enemyPrefab, posToSpawn, Quaternion.identity);
+
+            yield return _enemySpawnDelay;
+        }
+    }
+    
+}
