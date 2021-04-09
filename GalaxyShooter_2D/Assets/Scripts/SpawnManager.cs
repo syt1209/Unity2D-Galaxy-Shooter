3e522d5bb30d3438addff04263a16915762be481
syt1209 <syt1209@gmail.com>
2021-04-09 12:05:44 -0400

    Spawn routine for powerup set up

--- GalaxyShooter_2D/Assets/Scripts/SpawnManager.cs
+++ GalaxyShooter_2D/Assets/Scripts/SpawnManager.cs
@@ -4,36 +4,54 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    // config variables
+    [SerializeField]
+    private WaitForSeconds _enemySpawnDelay = new WaitForSeconds(3.0f);
+    private WaitForSeconds _powerupSpawnDelay = new WaitForSeconds(7.0f);
+    private bool _stopSpawning = false;
 
     // cached reference
     [SerializeField]
     private GameObject _enemyPrefab;
     private Transform _enemyContainer;
-
-    // config variables
     [SerializeField]
-    private WaitForSeconds _enemySpawnDelay = new WaitForSeconds(3.0f);
-    private bool _stopSpawning = false;
+    private GameObject _tripleShotPowerupPrefab;
 
     // Start is called before the first frame update
     void Start()
     {
         _enemyContainer = transform.Find("EnemyContainer").GetComponent<Transform>();
         StartCoroutine(EnemySpawnRoutine());
+        StartCoroutine(PowerUpSpawnRoutine());
     }
 
+    // private methods
+
+    // Enemy related
     private IEnumerator EnemySpawnRoutine()
     {
         while (_stopSpawning is false)
         {
             Vector3 posToSpawn = new Vector3(Random.Range(-8f, 8f), 7, 0);
             GameObject _newEnemy = Instantiate(_enemyPrefab, posToSpawn, Quaternion.identity, _enemyContainer);
-            //_newEnemy.transform.parent = _enemyContainer;
 
             yield return _enemySpawnDelay;
         }
     }
 
+    // Powerup related
+    private IEnumerator PowerUpSpawnRoutine()
+    {
+        while (_stopSpawning is false)
+        { 
+            Vector3 posToSpawn = new Vector3(Random.Range(-8f, 8f), 7, 0);
+            GameObject _newTripleShotPowerup = Instantiate(_tripleShotPowerupPrefab, posToSpawn, Quaternion.identity);
+
+            yield return _powerupSpawnDelay;
+        }
+    }
+
+    // public methods
     public void OnPlayerDeath()
     {
         _stopSpawning = true;
